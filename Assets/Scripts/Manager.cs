a740eab71b0571d975f4229b7acd0af45cdfcdf6
Alexis Bechet <fervac01@gmail.com>
2021-07-03 16:36:52 +0200

    Deserialized json user classes

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -44,6 +43,7 @@ public class Manager : MonoBehaviour
     public string login;
     public Image img;
     public Text UserErrorText;
+    public Text DisplayNameText;
 
     private void Awake()
     {
@@ -115,8 +115,7 @@ public class Manager : MonoBehaviour
             yield break;
         }
 
-        var user = JsonUtility.FromJson<UserObject>(www.downloadHandler.text);
-        
+        var user = JsonUtility.FromJson<RootObject>(www.downloadHandler.text);
 
         if (www.result == UnityWebRequest.Result.ConnectionError)
         {
@@ -125,6 +124,7 @@ public class Manager : MonoBehaviour
         }
         else
         {
+            DisplayNameText.text = user.displayname;
             StartCoroutine(LoadFromWeb(user.image_url));
         }
     }
@@ -177,11 +177,218 @@ public class Manager : MonoBehaviour
         public string created_at;
         public string updated_at;
     }
-    
 
-   [Serializable]
+
+    //[Serializable]
+    // public class RootObject
+    // {
+    //     public string users;
+    // }
+
+
+
+
+
+
+
+
+    [Serializable]
     public class RootObject
     {
-        public string users;
+        public int id;
+        public string email;
+        public string login;
+        public string first_name;
+        public string last_name;
+        public object usual_first_name;
+        public string url;
+        public string phone;
+        public string displayname;
+        public string usual_full_name;
+        public string image_url;
+        public bool staff;
+        public int correction_point;
+        public string pool_month;
+        public string pool_year;
+        public object location;
+        public int wallet;
+        public DateTime anonymize_date;
+        public DateTime created_at;
+        public DateTime updated_at;
+        public List<object> groups;
+        public List<CursusUsers> cursus_users;
+        public List<ProjectsUsers> projects_users;
+        public List<LanguagesUsers> languages_users;
+        public List<Achievement> achievements;
+        public List<object> titles;
+        public List<object> titles_users;
+        public List<object> partnerships;
+        public List<Patroned> patroned;
+        public List<object> patroning;
+        public List<object> expertises_users;
+        public List<object> roles;
+        public List<Campu> campus;
+        public List<CampusUsers> campus_users;
     }
+
+    [Serializable]
+    public class CursusUsers
+    {
+        public string grade;
+        public double level;
+        public List<Skill> skills;
+        public DateTime blackholed_at;
+        public int id;
+        public DateTime begin_at;
+        public object end_at;
+        public int cursus_id;
+        public bool has_coalition;
+        public DateTime created_at;
+        public DateTime updated_at;
+        public User user;
+        public Cursus cursus;
+    }
+
+    [Serializable]
+    public class Skill
+    {
+        public int id;
+        public string name;
+        public double level;
+    }
+
+    [Serializable]
+    public class User
+    {
+        public int id;
+        public string login;
+        public string url;
+        public DateTime created_at;
+        public DateTime updated_at;
+    }
+
+    [Serializable]
+    public class Cursus
+    {
+        public int id;
+        public DateTime created_at;
+        public string name;
+        public string slug;
+    }
+
+    [Serializable]
+    public class ProjectsUsers
+    {
+        public int id;
+        public int occurrence;
+        public int final_mark;
+        public string status;
+        public bool validated;
+        public int current_team_id;
+        public Project project;
+        public List<int> cursus_ids;
+        public DateTime marked_at;
+        public bool marked;
+        public DateTime retriable_at;
+        public DateTime created_at;
+        public DateTime updated_at;
+    }
+
+    [Serializable]
+    public class Project
+    {
+        public int id;
+        public string name;
+        public string slug;
+        public int parent_id;
+    }
+
+    [Serializable]
+    public class LanguagesUsers
+    {
+        public int id;
+        public int language_id;
+        public int user_id;
+        public int position;
+        public DateTime created_at;
+    }
+
+    [Serializable]
+    public class Achievement
+    {
+        public int id;
+        public string name;
+        public string description;
+        public string tier;
+        public string kind;
+        public bool visible;
+        public string image;
+        public object nbr_of_success;
+        public string users_url;
+    }
+
+    [Serializable]
+    public class Patroned
+    {
+        public int id;
+        public int user_id;
+        public int godfather_id;
+        public bool ongoing;
+        public DateTime created_at;
+        public DateTime updated_at;
+    }
+
+    [Serializable]
+    public class Campu
+    {
+        public int id;
+        public string name;
+        public string time_zone;
+        public Language language;
+        public int users_count;
+        public int vogsphere_id;
+        public string country;
+        public string address;
+        public string zip;
+        public string city;
+        public string website;
+        public string facebook;
+        public string twitter;
+        public bool active;
+        public string email_extension;
+        public bool default_hidden_phone;
+    }
+
+    [Serializable]
+    public class Language
+    {
+        public int id;
+        public string name;
+        public string identifier;
+        public DateTime created_at;
+        public DateTime updated_at;
+    }
+
+    [Serializable]
+    public class CampusUsers
+    {
+        public int id;
+        public int user_id;
+        public int campus_id;
+        public bool is_primary;
+        public DateTime created_at;
+        public DateTime updated_at;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
